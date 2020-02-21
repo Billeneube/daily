80de00a55004aeba0ec2fcd2da9b7519341d008b
jonas blanchard <43017245+Billeneube@users.noreply.github.com>
2020-02-21 12:20:37 -0800

    Create dailychallenge.py

---
+++ dailychallenge.py
@@ -0,0 +1,16 @@
+
+
+getRealFloor(1) = 1
+getRealFloor(13) = 13
+getRealfloor(2) = 2
+getRealFloor(0) = 0
+
+getRealFloor = keyPressed
+
+if (getRealFloor = 1) or (getRealFloor = 13) or (getRealFloor = 0)
+  print(0)
+  
+ else
+  print((getRealFloor)+1)
+ 
+ 
